db7cab1fa35ca7afd99815e0b3d3ceba226ab119
Raji <raji.arora@gmail.com>
2015-05-08 18:27:09 -0500

    spring demo

--- server.R
+++ server.R
@@ -8,15 +8,10 @@ exp = t(exprs(miEset))
 #rows are different cell lines and columns are miRNAs (they are like genes)
 #cell lines are from different cancer types
 # exp is expression value of miRNAs in these cell lines
-
-#PCA, but will have to play with it a little, not very good right now
-p = prcomp(exp)
-pc=p
-names(p)
-#change rownames of x from names of cell lines to names of tissues 
-rownames(p$x) = pData(miEset)$tissue.name
+rownames(exp)<-pData(miEset)$tissue.name
+sortedgroup<-names(sort(table(pData(miEset)$tissue.name),dec=TRUE))
 as.fumeric <- function(x,levels=unique(x)) as.numeric(factor(x,levels=levels))
-group = as.fumeric(rownames(p$x))
+
 #all data
 
 library(calibrate)
@@ -25,11 +20,22 @@ shinyServer(function(input, output) {
   
   output$scatterPlot <- renderPlot({
     numpoints <- input$n
-    plot(pc$x[0:numpoints,1], pc$x[0:numpoints,2], main = "PCA", xlab = "PC1", ylab = "PC2", col=group)
-    if (numpoints <= 50)
-    {
-      textxy(pc$x[0:numpoints, 1], pc$x[0:numpoints, 2],rownames(p$x)[51:100])
-    }
+    topnames<-head((sortedgroup),numpoints)
+  
+    #PCA, but will have to play with it a little, not very good right now
+    sexp<-exp[rownames(exp) %in% topnames,]
+  
+    psexp<-prcomp(sexp)
+  
+    #change rownames of x from names of cell lines to names of tissues 
+    rownames(psexp$x)<-rownames(sexp)
+  
+     plotpointsx<-psexp$x[,1]
+    plotpointsy<-psexp$x[,2]
+    print(length(plotpointsx))
+    colorsgroup=as.fumeric(topnames)
+    plot(plotpointsx, plotpointsy, main = "PCA", xlab = "PC1", ylab = "PC2", pch=16,col=colorsgroup)
+    legend("topright",topnames,col=colorsgroup,pch=16)
   })
   
 })
--- ui.R
+++ ui.R
@@ -2,20 +2,20 @@ library(shiny)
 
 shinyUI(fluidPage(
   
-  titlePanel("Conditional panels"),
+  titlePanel("Interactive PCA Analysis of Sanger miRNA Data"),
   
   column(4, wellPanel(
-    sliderInput("n", "Number of points:",
-                min = 10, max = 550, value = 10, step = 10)
+    sliderInput("n", "Number of cell lines(sorted by number of samples):",
+                min = 1, max = 20, value = 10, step = 1)
   )),
   
   column(5,
-         "PCA Plot for Cancer Data",
+         "",
          
          # With the conditionalPanel, the condition is a JavaScript
          # expression. In these expressions, input values like
          # input$n are accessed with dots, as in input.n
-         conditionalPanel("input.n >= 10",
+         conditionalPanel("input.n >= 1",
                           plotOutput("scatterPlot", height = 640, width=640)
          )
   )
